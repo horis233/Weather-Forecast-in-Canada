4dde38af5a1c77024db262b5966cf8a7bd1c510c
horis233 <horis233@gmail.com>
2018-07-23 00:28:49 -0400

    change weather forecast area

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
-const API_KEY = '73dcba9983ce07935c181db05f788874';
+const API_KEY = '6c446cd3d9e25dedb8440db541f81657';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q= ${city}, us`;
+  const url = `${ROOT_URL}&q= ${city}, ca`;
   const request = axios.get(url);
 
   console.log("Request: ", request);
-  
+
   return {
       type: FETCH_WEATHER,
       payload: request
